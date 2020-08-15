a36ea33722cd94a74f66a8453970a9441ff4a7c5
Jacob Cowan <mrjcowman@gmail.com>
2020-08-15 02:37:11 -0700

    Implemented return button

--- printScript.js
+++ printScript.js
@@ -1,36 +1,3 @@
-function printDiv(divName){
-    const div = document.getElementById(divName);
-    const printContents = div.innerHTML;
-    const originalContents = document.body.innerHTML;
-
-    // document.body.innerHTML = printContents;
-    // document.body.setAttribute("class", "page");
-
-    // print();
-
-    // document.body.removeAttribute("class");
-
-    document.body.style.visibility = "hidden";
-
-    let children = div.children;
-    const numChildren = children.length;
-
-    for(let i=0; i<numChildren; i++){
-        children[i].style.visibility = "visible";
-    }
-
-    div.setAttribute("class", "page printedPage");
-
-    print();
-
-    div.setAttribute("class", "page");
-
-    document.body.style.visibility="visible";
-
-
-    // document.body.innerHTML = originalContents;
-}
-
 function printDivs(divClass){
     const divs = document.getElementsByClassName(divClass);
     document.body.style.visibility = "hidden";
